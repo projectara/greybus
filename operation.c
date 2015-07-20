projectara/greybus

commit 9218fac2a24d798a3a4764fe11209c3a012031a5

operation: allow atomic request submissions
Add gfp mask argument to gb_operation_request_send in order to allow
submissions from atomic context.

Note that responses are currently always sent from non-atomic context as
incoming requests are processed in a work queue.

Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

operation.c
@@ -129,7 +129,7 @@ gb_operation_find(struct gb_connection *connection, u16 operation_id)
 	return found ? operation : NULL;
 }
 
-static int gb_message_send(struct gb_message *message)
+static int gb_message_send(struct gb_message *message, gfp_t gfp)
 {
 	struct gb_connection *connection = message->operation->connection;
 
@@ -563,7 +563,8 @@ static void gb_operation_sync_callback(struct gb_operation *operation)
  * dropping the initial reference to the operation.
  */
 int gb_operation_request_send(struct gb_operation *operation,
-				gb_operation_callback callback)
+				gb_operation_callback callback,
+				gfp_t gfp)
 {
 	struct gb_connection *connection = operation->connection;
 	struct gb_operation_msg_hdr *header;
@@ -601,7 +602,7 @@ int gb_operation_request_send(struct gb_operation *operation,
 	/* All set, send the request */
 	gb_operation_result_set(operation, -EINPROGRESS);
 
-	ret = gb_message_send(operation->request);
+	ret = gb_message_send(operation->request, gfp);
 	if (ret)
 		gb_operation_put(operation);
 
@@ -620,7 +621,8 @@ int gb_operation_request_send_sync(struct gb_operation *operation)
 	int ret;
 	unsigned long timeout;
 
-	ret = gb_operation_request_send(operation, gb_operation_sync_callback);
+	ret = gb_operation_request_send(operation, gb_operation_sync_callback,
+					GFP_KERNEL);
 	if (ret)
 		return ret;
 
@@ -677,7 +679,7 @@ int gb_operation_response_send(struct gb_operation *operation, int errno)
 	/* Fill in the response header and send it */
 	operation->response->header->result = gb_operation_errno_map(errno);
 
-	ret = gb_message_send(operation->response);
+	ret = gb_message_send(operation->response, GFP_KERNEL);
 	if (ret)
 		gb_operation_put(operation);
 

operation.h
@@ -147,7 +147,8 @@ bool gb_operation_response_alloc(struct gb_operation *operation,
 					size_t response_size);
 
 int gb_operation_request_send(struct gb_operation *operation,
-				gb_operation_callback callback);
+				gb_operation_callback callback,
+				gfp_t gfp);
 int gb_operation_request_send_sync(struct gb_operation *operation);
 int gb_operation_response_send(struct gb_operation *operation, int errno);
 

commit 26d41f24b3731556d6ba3bf433a4c2e21e535cdb

operation: fix atomic message submission
The recently added GFP-flags argument to gb_message_send was never used.

Fixes: 9218fac2a24d ("operation: allow atomic request submissions")
Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

operation.c
@@ -200,7 +200,7 @@ static int gb_message_send(struct gb_message *message, gfp_t gfp)
 	return connection->hd->driver->message_send(connection->hd,
 					connection->hd_cport_id,
 					message,
-					GFP_KERNEL);
+					gfp);
 }
 
 /*