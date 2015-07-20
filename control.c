projectara/greybus

commit 9aa174d202e572370d2e51f5ba459004957ad935

operation: allow atomic operation allocations
Add gfp mask argument to gb_operation_create to allow operations to be
allocated in atomic context.

Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

operation.c
@@ -409,22 +409,13 @@ EXPORT_SYMBOL_GPL(gb_operation_response_alloc);
  */
 static struct gb_operation *
 gb_operation_create_common(struct gb_connection *connection, u8 type,
-				size_t request_size, size_t response_size)
+				size_t request_size, size_t response_size,
+				gfp_t gfp_flags)
 {
 	struct greybus_host_device *hd = connection->hd;
 	struct gb_operation *operation;
 	unsigned long flags;
-	gfp_t gfp_flags;
 
-	/*
-	 * An incoming request will pass an invalid operation type,
-	 * because the header will get overwritten anyway.  These
-	 * occur in interrupt context, so we must use GFP_ATOMIC.
-	 */
-	if (type == GB_OPERATION_TYPE_INVALID)
-		gfp_flags = GFP_ATOMIC;
-	else
-		gfp_flags = GFP_KERNEL;
 	operation = kmem_cache_zalloc(gb_operation_cache, gfp_flags);
 	if (!operation)
 		return NULL;
@@ -472,7 +463,8 @@ err_cache:
  */
 struct gb_operation *gb_operation_create(struct gb_connection *connection,
 					u8 type, size_t request_size,
-					size_t response_size)
+					size_t response_size,
+					gfp_t gfp)
 {
 	if (WARN_ON_ONCE(type == GB_OPERATION_TYPE_INVALID))
 		return NULL;
@@ -480,7 +472,7 @@ struct gb_operation *gb_operation_create(struct gb_connection *connection,
 		type &= ~GB_MESSAGE_TYPE_RESPONSE;
 
 	return gb_operation_create_common(connection, type,
-					request_size, response_size);
+					request_size, response_size, gfp);
 }
 EXPORT_SYMBOL_GPL(gb_operation_create);
 
@@ -504,7 +496,7 @@ gb_operation_create_incoming(struct gb_connection *connection, u16 id,
 
 	operation = gb_operation_create_common(connection,
 					GB_OPERATION_TYPE_INVALID,
-					request_size, 0);
+					request_size, 0, GFP_ATOMIC);
 	if (operation) {
 		operation->id = id;
 		operation->type = type;
@@ -888,7 +880,8 @@ int gb_operation_sync(struct gb_connection *connection, int type,
 		return -EINVAL;
 
 	operation = gb_operation_create(connection, type,
-					request_size, response_size);
+					request_size, response_size,
+					GFP_KERNEL);
 	if (!operation)
 		return -ENOMEM;
 

operation.h
@@ -134,7 +134,8 @@ int gb_operation_result(struct gb_operation *operation);
 size_t gb_operation_get_payload_size_max(struct gb_connection *connection);
 struct gb_operation *gb_operation_create(struct gb_connection *connection,
 					u8 type, size_t request_size,
-					size_t response_size);
+					size_t response_size,
+					gfp_t gfp);
 void gb_operation_get(struct gb_operation *operation);
 void gb_operation_put(struct gb_operation *operation);
 static inline void gb_operation_destroy(struct gb_operation *operation)

hid.c
@@ -128,7 +128,8 @@ static int gb_hid_set_report(struct gb_hid *ghid, u8 report_type, u8 report_id,
 	int ret, size = sizeof(*request) + len - 1;
 
 	operation = gb_operation_create(ghid->connection,
-					GB_HID_TYPE_SET_REPORT, size, 0);
+					GB_HID_TYPE_SET_REPORT, size, 0,
+					GFP_KERNEL);
 	if (!operation)
 		return -ENOMEM;
 

i2c.c
@@ -146,7 +146,7 @@ gb_i2c_operation_create(struct gb_connection *connection,
 
 	/* Response consists only of incoming data */
 	operation = gb_operation_create(connection, GB_I2C_TYPE_TRANSFER,
-				request_size, data_in_size);
+				request_size, data_in_size, GFP_KERNEL);
 	if (!operation)
 		return NULL;
 

spi.c
@@ -90,7 +90,7 @@ gb_spi_operation_create(struct gb_connection *connection,
 
 	/* Response consists only of incoming data */
 	operation = gb_operation_create(connection, GB_SPI_TYPE_TRANSFER,
-					request_size, rx_size);
+					request_size, rx_size, GFP_KERNEL);
 	if (!operation)
 		return NULL;
 

usb.c
@@ -131,7 +131,8 @@ static int urb_enqueue(struct usb_hcd *hcd, struct urb *urb, gfp_t mem_flags)
 	operation = gb_operation_create(dev->connection,
 					GB_USB_TYPE_URB_ENQUEUE,
 					sizeof(*request) +
-					urb->transfer_buffer_length, 0);
+					urb->transfer_buffer_length, 0,
+					GFP_KERNEL);
 	if (!operation)
 		return -ENODEV;
 

commit 359875228a7df926d16fd9de84e0cb8f73784b64

operation: fix atomic response allocation
Response allocation also needs a GFP-flags argument as a response is
allocated as part of an outgoing operation.

Fixes: 9aa174d202e5 ("operation: allow atomic operation allocations")
Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

control.c
@@ -75,7 +75,8 @@ static int gb_control_request_recv(u8 type, struct gb_operation *op)
 		// an AP.
 		break;
 	case GB_CONTROL_TYPE_PROTOCOL_VERSION:
-		if (!gb_operation_response_alloc(op, sizeof(*version))) {
+		if (!gb_operation_response_alloc(op, sizeof(*version),
+						 GFP_KERNEL)) {
 			dev_err(&connection->dev,
 				"%s: error allocating response\n", __func__);
 			return -ENOMEM;

loopback.c
@@ -283,7 +283,8 @@ static int gb_loopback_request_recv(u8 type, struct gb_operation *operation)
 		}
 
 		if (len) {
-			if (!gb_operation_response_alloc(operation, len)) {
+			if (!gb_operation_response_alloc(operation, len,
+							 GFP_KERNEL)) {
 				dev_err(&connection->dev,
 					"error allocating response\n");
 				return -ENOMEM;

operation.c
@@ -426,7 +426,7 @@ static u8 gb_operation_errno_map(int errno)
 }
 
 bool gb_operation_response_alloc(struct gb_operation *operation,
-					size_t response_size)
+					size_t response_size, gfp_t gfp)
 {
 	struct greybus_host_device *hd = operation->connection->hd;
 	struct gb_operation_msg_hdr *request_header;
@@ -434,8 +434,7 @@ bool gb_operation_response_alloc(struct gb_operation *operation,
 	u8 type;
 
 	type = operation->type | GB_MESSAGE_TYPE_RESPONSE;
-	response = gb_operation_message_alloc(hd, type, response_size,
-						GFP_KERNEL);
+	response = gb_operation_message_alloc(hd, type, response_size, gfp);
 	if (!response)
 		return false;
 	response->operation = operation;
@@ -497,8 +496,10 @@ gb_operation_create_common(struct gb_connection *connection, u8 type,
 
 	/* Allocate the response buffer for outgoing operations */
 	if (!(op_flags & GB_OPERATION_FLAG_INCOMING)) {
-		if (!gb_operation_response_alloc(operation, response_size))
+		if (!gb_operation_response_alloc(operation, response_size,
+						 gfp_flags)) {
 			goto err_request;
+		}
 	}
 
 	operation->flags = op_flags;
@@ -734,7 +735,7 @@ static int gb_operation_response_send(struct gb_operation *operation,
 
 	if (!operation->response &&
 			!gb_operation_is_unidirectional(operation)) {
-		if (!gb_operation_response_alloc(operation, 0))
+		if (!gb_operation_response_alloc(operation, 0, GFP_KERNEL))
 			return -ENOMEM;
 	}
 

operation.h
@@ -166,7 +166,7 @@ static inline void gb_operation_destroy(struct gb_operation *operation)
 }
 
 bool gb_operation_response_alloc(struct gb_operation *operation,
-					size_t response_size);
+					size_t response_size, gfp_t gfp);
 
 int gb_operation_request_send(struct gb_operation *operation,
 				gb_operation_callback callback,