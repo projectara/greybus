projectara/greybus

commit 1f92f6404614533a4a39099805674afe1dafe106

core: return error code when creating host device
Return a pointer-coded error from greybus_create_hd() rather
than NULL in the event an error occurs.

Signed-off-by: Alex Elder <elder@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

core.c
@@ -187,7 +187,7 @@ struct greybus_host_device *greybus_create_hd(struct greybus_host_driver *driver
 	if ((!driver->message_send) || (!driver->message_cancel) ||
 	    (!driver->submit_svc)) {
 		pr_err("Must implement all greybus_host_driver callbacks!\n");
-		return NULL;
+		return ERR_PTR(-EINVAL);
 	}
 
 	/*
@@ -202,7 +202,7 @@ struct greybus_host_device *greybus_create_hd(struct greybus_host_driver *driver
 
 	hd = kzalloc(sizeof(*hd) + driver->hd_priv_size, GFP_KERNEL);
 	if (!hd)
-		return NULL;
+		return ERR_PTR(-ENOMEM);
 
 	kref_init(&hd->kref);
 	hd->parent = parent;
@@ -215,7 +215,7 @@ struct greybus_host_device *greybus_create_hd(struct greybus_host_driver *driver
 	endo = gb_endo_create(hd, endo_id);
 	if (IS_ERR(endo)) {
 		greybus_remove_hd(hd);
-		return NULL;
+		return ERR_CAST(endo);
 	}
 	hd->endo = endo;
 

es1.c
@@ -556,9 +556,9 @@ static int ap_probe(struct usb_interface *interface,
 	udev = usb_get_dev(interface_to_usbdev(interface));
 
 	hd = greybus_create_hd(&es1_driver, &udev->dev, ES1_GBUF_MSG_SIZE_MAX);
-	if (!hd) {
+	if (IS_ERR(hd)) {
 		usb_put_dev(udev);
-		return -ENOMEM;
+		return PTR_ERR(hd);
 	}
 
 	es1 = hd_to_es1(hd);

es2.c
@@ -556,9 +556,9 @@ static int ap_probe(struct usb_interface *interface,
 	udev = usb_get_dev(interface_to_usbdev(interface));
 
 	hd = greybus_create_hd(&es1_driver, &udev->dev, ES1_GBUF_MSG_SIZE_MAX);
-	if (!hd) {
+	if (IS_ERR(hd)) {
 		usb_put_dev(udev);
-		return -ENOMEM;
+		return PTR_ERR(hd);
 	}
 
 	es1 = hd_to_es1(hd);

commit 09ec9c7073d1c93b265c069dd6068fe843d3a359

core: fix hd-creation error path
Make sure to return an errno when a host-device buffer-size check fails.

Fixes: 1f92f6404614 ("core: return error code when creating host device")
Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>

core.c
@@ -184,7 +184,7 @@ struct greybus_host_device *greybus_create_hd(struct greybus_host_driver *driver
 
 	if (buffer_size_max < GB_OPERATION_MESSAGE_SIZE_MIN) {
 		dev_err(parent, "greybus host-device buffers too small\n");
-		return NULL;
+		return ERR_PTR(-EINVAL);
 	}
 
 	if (num_cports == 0 || num_cports > CPORT_ID_MAX) {