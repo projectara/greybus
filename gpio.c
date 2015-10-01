projectara/greybus

commit e7895cfc476c38f033715774cc77c6383288cc03

gpio: handle api change in generic_handle_irq_desc()
generic_handle_irq_desc changed the api in the 4.2 kernel, so fix up the
gpio driver to handle this properly to keep it working.

Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

gpio.c
@@ -384,7 +384,11 @@ static int gb_gpio_request_recv(u8 type, struct gb_operation *op)
 	}
 
 	local_irq_disable();
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0)
 	generic_handle_irq_desc(irq, desc);
+#else
+	generic_handle_irq_desc(desc);
+#endif
 	local_irq_enable();
 
 	return 0;

commit 493dc74085ed69a1343ea465deff2bb8b326781f

gpio: fix generic_handle_irq_desc api change
The generic_handle_irq_desc api only have changed in 4.3.0, so check
against the correct version, if not will break builds for 4.2.x.

Fixes: e7895cfc476 ("gpio: handle api change in generic_handle_irq_desc()")
Signed-off-by: Rui Miguel Silva <rui.silva@linaro.org>
Reviewed-by: Johan Hovold <johan@hovoldconsulting.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

gpio.c
@@ -384,7 +384,7 @@ static int gb_gpio_request_recv(u8 type, struct gb_operation *op)
 	}
 
 	local_irq_disable();
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 2, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 3, 0)
 	generic_handle_irq_desc(irq, desc);
 #else
 	generic_handle_irq_desc(desc);