1888a2463a4e4db7115004d4ff67151c90afb50c
Greg Kroah-Hartman <gregkh@google.com>
2016-01-21 09:58:42 -0800

    Revert "connection: remove skip-connected legacy protocol flags"
    This reverts commit a2aa1ba5c01d8d8d952c50349276d25f2ccba396.
    
    We don't want it in the demo branch right now...
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- connection.c
+++ connection.c
@@ -295,17 +295,15 @@ gb_connection_svc_connection_destroy(struct gb_connection *connection)
 /* Inform Interface about active CPorts */
 static int gb_connection_control_connected(struct gb_connection *connection)
 {
+	struct gb_protocol *protocol = connection->protocol;
 	struct gb_control *control;
 	u16 cport_id = connection->intf_cport_id;
 	int ret;
 
-	if (gb_connection_is_static(connection))
+	if (protocol->flags & GB_PROTOCOL_SKIP_CONTROL_CONNECTED)
 		return 0;
 
-	control = connection->intf->control;
-
-	if (connection == control->connection)
-		return 0;
+	control = connection->bundle->intf->control;
 
 	ret = gb_control_connected_operation(control, cport_id);
 	if (ret) {
@@ -321,17 +319,15 @@ static int gb_connection_control_connected(struct gb_connection *connection)
 static void
 gb_connection_control_disconnected(struct gb_connection *connection)
 {
+	struct gb_protocol *protocol = connection->protocol;
 	struct gb_control *control;
 	u16 cport_id = connection->intf_cport_id;
 	int ret;
 
-	if (gb_connection_is_static(connection))
+	if (protocol->flags & GB_PROTOCOL_SKIP_CONTROL_DISCONNECTED)
 		return;
 
-	control = connection->intf->control;
-
-	if (connection == control->connection)
-		return;
+	control = connection->bundle->intf->control;
 
 	ret = gb_control_disconnected_operation(control, cport_id);
 	if (ret) {
