projectara/greybus

commit cb0ef0c019ab586a8af234d150f506b1bc92164b

operation: print message type on errors
This can be very useful debug information, print it.

Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

operation.c
@@ -235,8 +235,8 @@ static void gb_operation_request_handle(struct gb_operation *operation)
 	ret = gb_operation_response_send(operation, status);
 	if (ret) {
 		dev_err(&operation->connection->dev,
-			"failed to send response %d: %d\n",
-			status, ret);
+			"failed to send response %d for type 0x%02hhx: %d\n",
+			status, operation->type, ret);
 			return;
 	}
 }
@@ -794,7 +794,8 @@ void greybus_message_sent(struct greybus_host_device *hd,
 	if (message == operation->response) {
 		if (status) {
 			dev_err(&connection->dev,
-				"error sending response: %d\n", status);
+				"error sending response type 0x%02hhx: %d\n",
+				operation->type, status);
 		}
 		gb_operation_put_active(operation);
 		gb_operation_put(operation);
@@ -867,8 +868,8 @@ static void gb_connection_recv_response(struct gb_connection *connection,
 	message = operation->response;
 	message_size = sizeof(*message->header) + message->payload_size;
 	if (!errno && size != message_size) {
-		dev_err(&connection->dev, "bad message size (%zu != %zu)\n",
-			size, message_size);
+		dev_err(&connection->dev, "bad message (0x%02hhx) size (%zu != %zu)\n",
+			size, message_size, message->header->type);
 		errno = -EMSGSIZE;
 	}
 
@@ -913,8 +914,9 @@ void gb_connection_recv(struct gb_connection *connection,
 	msg_size = le16_to_cpu(header.size);
 	if (size < msg_size) {
 		dev_err(&connection->dev,
-			"incomplete message received: 0x%04x (%zu < %zu)\n",
-			le16_to_cpu(header.operation_id), size, msg_size);
+			"incomplete message received for type 0x%02hhx: 0x%04x (%zu < %zu)\n",
+			header.type, le16_to_cpu(header.operation_id), size,
+			msg_size);
 		return;		/* XXX Should still complete operation */
 	}
 
@@ -1019,8 +1021,8 @@ int gb_operation_sync_timeout(struct gb_connection *connection, int type,
 
 	ret = gb_operation_request_send_sync_timeout(operation, timeout);
 	if (ret) {
-		dev_err(&connection->dev, "synchronous operation failed: %d\n",
-			ret);
+		dev_err(&connection->dev, "synchronous operation failed: 0x%02hhx (%d)\n",
+			type, ret);
 	} else {
 		if (response_size) {
 			memcpy(response, operation->response->payload,

commit 9106691e95063011635e8d10daa34d80bea2ea1c

SW-2700: operation: fix broken response error messages
The operation type included in the error message printed for malformed
responses has never been correct. An uninitialised buffer was used to
retrieve the type, resulting in the type always being reported as 0.

Fix this by passing a properly aligned header to the response handler,
and drop the now redundant id and result parameters.

Fixes: cb0ef0c019ab ("operation: print message type on errors")
Reviewed-by: Viresh Kumar <viresh.kumar@linaro.org>
Reviewed-by: Laurent Pinchart <laurent.pinchart@linaro.org>
Signed-off-by: Johan Hovold <johan@hovoldconsulting.com>
Reviewed-by: Alex Elder <elder@linaro.org>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

operation.c
@@ -928,13 +928,16 @@ static void gb_connection_recv_request(struct gb_connection *connection,
  * data into the response buffer and handle the rest via workqueue.
  */
 static void gb_connection_recv_response(struct gb_connection *connection,
-			u16 operation_id, u8 result, void *data, size_t size)
+				const struct gb_operation_msg_hdr *header,
+				void *data, size_t size)
 {
-	struct gb_operation_msg_hdr *header;
 	struct gb_operation *operation;
 	struct gb_message *message;
-	int errno = gb_operation_status_map(result);
 	size_t message_size;
+	u16 operation_id;
+	int errno;
+
+	operation_id = le16_to_cpu(header->operation_id);
 
 	if (!operation_id) {
 		dev_err_ratelimited(&connection->hd->dev,
@@ -951,8 +954,8 @@ static void gb_connection_recv_response(struct gb_connection *connection,
 		return;
 	}
 
+	errno = gb_operation_status_map(header->result);
 	message = operation->response;
-	header = message->header;
 	message_size = sizeof(*header) + message->payload_size;
 	if (!errno && size > message_size) {
 		dev_err_ratelimited(&connection->hd->dev,
@@ -979,7 +982,7 @@ static void gb_connection_recv_response(struct gb_connection *connection,
 
 	/* The rest will be handled in work queue context */
 	if (gb_operation_result_set(operation, errno)) {
-		memcpy(header, data, size);
+		memcpy(message->buffer, data, size);
 		queue_work(gb_operation_completion_wq, &operation->work);
 	}
 
@@ -1026,8 +1029,8 @@ void gb_connection_recv(struct gb_connection *connection,
 
 	operation_id = le16_to_cpu(header.operation_id);
 	if (header.type & GB_MESSAGE_TYPE_RESPONSE)
-		gb_connection_recv_response(connection, operation_id,
-						header.result, data, msg_size);
+		gb_connection_recv_response(connection,	&header, data,
+						msg_size);
 	else
 		gb_connection_recv_request(connection, operation_id,
 						header.type, data, msg_size);